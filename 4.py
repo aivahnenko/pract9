54bf79977f78f8acf133efc0e451a80de794c133
aivahnenko <aivahnenko866@gmail.com>
2025-12-15 09:42:17 +0300

    4.py

--- 4.py
+++ 4.py
@@ -7,12 +7,12 @@ count_last = 0
 count_even = 0
 sum_over5 = 0
 product_over7 = 1
-found_over7 = False
+count_over7 = 0
 count_0_5 = 0
 
 while original > 0:
     digit = original % 10
-    
+
     if digit == 3:
         count_3 += 1
     if digit == last_digit:
@@ -23,14 +23,14 @@ while original > 0:
         sum_over5 += digit
     if digit > 7:
         product_over7 *= digit
-        found_over7 = True
+        count_over7 += 1
     if digit == 0 or digit == 5:
         count_0_5 += 1
-        
+
     original //= 10
 
-if not found_over7:
-    product_over7 = 1
+if count_over7 == 0:
+    product_over7 = 0
 
 print(f"Количество цифр 3: {count_3}")
 print(f"Количество последних цифр: {count_last}")
