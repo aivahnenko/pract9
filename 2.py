059af75333c65e0481b5e0ce64b5aad8ac83b742
aivahnenko <aivahnenko866@gmail.com>
2025-12-22 09:30:15 +0300

    2.py

--- 2.py
+++ 2.py
@@ -1,17 +1,18 @@
-print("Введи имена, пустая строка - стоп")
-name = input("Имя: ")
+print('Введи имена, пустая строка - стоп')
+name = input('Имя: ')
 nomer = 0
 a_gde = -1
 l_gde = -1
 while name != "":
-    if name == "Александра":
+    if name == 'Александра':
         a_gde = nomer
-    if name == "Левон":
+    if name == 'Левон':
         l_gde = nomer
     nomer += 1
-    name = input("Имя: ")
+    name = input('Имя: ')
 if a_gde >= 0 and l_gde > a_gde:
     mezhdu = l_gde - a_gde - 1
 else:
     mezhdu = 0
-print("Между ними:", mezhdu)
+
+print(f'Между ними {mezhdu}')
