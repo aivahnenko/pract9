2f8ded00ca893b2d2b44bb5b2aa893a8927d854f
aivahnenko <aivahnenko866@gmail.com>
2025-12-22 11:33:35 +0300

    3.py

--- 3.py
+++ 3.py
@@ -1,16 +1,20 @@
 price = int(input('Сколько надо заплатить '))
 vsego = 0
 
-vsego += price // 25
-price = price % 25
+while price >= 25:
+    vsego += price // 25
+    price = price % 25
 
-vsego += price // 10
-price = price % 10
+while price >= 10:
+    vsego += price // 10
+    price = price % 10
 
-vsego += price // 5
-price = price % 5
+while price >= 5:
+    vsego += price // 5
+    price = price % 5
 
-vsego += price // 1
+while price >= 1:
+    vsego += price // 1
+    price = price % 1
 
 print(f'Нужно монет {vsego}')
-
